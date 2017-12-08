2605360e77d4f0bb4b91c55a74de99029dd5248e
CalebHarsh <caleb.harsh139@gmail.com>
2017-12-07 21:25:08 -0700

    Outline API setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,22 @@
- // Initialize Firebase
- var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyCsJBnT5G6vN1tFi3oamE0cNXkvM0NNUcM",
     authDomain: "event-planner-54834.firebaseapp.com",
     databaseURL: "https://event-planner-54834.firebaseio.com",
     projectId: "event-planner-54834",
     storageBucket: "event-planner-54834.appspot.com",
     messagingSenderId: "803664413287"
-  };
-  firebase.initializeApp(config);
+};
+
+//config firebase
+firebase.initializeApp(config);
+
+//get access to firebase database
+var database = firebase.database();
+
+//Weather API
+
+//Eventbrite API
+
+//Location Auto-complete API
+

a2ac639065a7bca1eda89d1faf3d92e826ae495f
CalebHarsh <caleb.harsh139@gmail.com>
2017-12-07 21:17:18 -0700

    Added firebase config and script tag

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCsJBnT5G6vN1tFi3oamE0cNXkvM0NNUcM",
+    authDomain: "event-planner-54834.firebaseapp.com",
+    databaseURL: "https://event-planner-54834.firebaseio.com",
+    projectId: "event-planner-54834",
+    storageBucket: "event-planner-54834.appspot.com",
+    messagingSenderId: "803664413287"
+  };
+  firebase.initializeApp(config);

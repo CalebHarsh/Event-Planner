4b405e46d9039a8dd8ade07f8ea2760610c8f6f0
JamieDavid <jjdavid14@live.com>
2017-12-09 11:50:14 -0700

    finished input form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,23 @@
+// // Initialize Firebase
+// var config = {
+//     apiKey: "AIzaSyCsJBnT5G6vN1tFi3oamE0cNXkvM0NNUcM",
+//     authDomain: "event-planner-54834.firebaseapp.com",
+//     databaseURL: "https://event-planner-54834.firebaseio.com",
+//     projectId: "event-planner-54834",
+//     storageBucket: "event-planner-54834.appspot.com",
+//     messagingSenderId: "803664413287"
+// };
+
+// MY LOCALHOST TESTING
 // Initialize Firebase
-var config = {
-    apiKey: "AIzaSyCsJBnT5G6vN1tFi3oamE0cNXkvM0NNUcM",
-    authDomain: "event-planner-54834.firebaseapp.com",
-    databaseURL: "https://event-planner-54834.firebaseio.com",
-    projectId: "event-planner-54834",
-    storageBucket: "event-planner-54834.appspot.com",
-    messagingSenderId: "803664413287"
-};
+  var config = {
+    apiKey: "AIzaSyCGj0VzsfaUXPfXK1yUCjyLU8imQw7SBbs",
+    authDomain: "event-planner-6c6a9.firebaseapp.com",
+    databaseURL: "https://event-planner-6c6a9.firebaseio.com",
+    projectId: "event-planner-6c6a9",
+    storageBucket: "event-planner-6c6a9.appspot.com",
+    messagingSenderId: "168847381672"
+  };
 
 //config firebase
 firebase.initializeApp(config);
@@ -19,4 +30,3 @@ var database = firebase.database();
 //Eventbrite API
 
 //Location Auto-complete API
-
